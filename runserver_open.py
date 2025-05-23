66431bf426f11c5f85a5127d7600e799aa70adff
cajango-dev <cajango.dev@gmail.com>
2025-05-23 11:22:41 -0300

    commit 23/05

--- runserver_open.py
+++ runserver_open.py
@@ -4,21 +4,14 @@ import time
 import sys
 import os
 
-# Ajuste aqui o caminho do python dentro do seu venv
-# Exemplo típico no Windows:
 venv_python = os.path.join("venv", "Scripts", "python.exe")
 
-# URL para abrir
 url = "http://127.0.0.1:8000/admin/login/?next=/admin/"
 
-# Inicia o servidor Django usando o python do venv
 server = subprocess.Popen([venv_python, "manage.py", "runserver"])
 
-# Espera o servidor subir (pode ajustar o tempo, dependendo da máquina)
 time.sleep(3)
 
-# Abre o navegador na URL desejada
 webbrowser.open(url)
 
-# Aguarda o servidor rodar até ser interrompido (Ctrl+C)
 server.wait()
